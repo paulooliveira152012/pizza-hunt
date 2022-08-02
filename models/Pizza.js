a87c3f400b38d3b4bba5316e2197e396e2b35f1b
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-08-02 14:09:33 -0400

    done

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,24 +1,27 @@
-//import dependencies
 const { Schema, model } = require('mongoose');
-//import function for time format
 const dateFormat = require('../utils/dateFormat');
 
-//creating pizza schema
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal)
+      get: createdAtVal => dateFormat(createdAtVal)
     },
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large'
     },
     toppings: [],
@@ -34,17 +37,19 @@ const PizzaSchema = new Schema(
       virtuals: true,
       getters: true
     },
+    // prevents virtuals from creating duplicate of _id as `id`
     id: false
   }
 );
 
-  // get total count of comments and replies on retrieval
+// get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
-  // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-// export the Pizza model
 module.exports = Pizza;
